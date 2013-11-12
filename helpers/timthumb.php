1867eed5b2cc48eeb03b724043ef290de651084b
Mark Steadman <marksteadman@me.com>
2013-11-12 10:57:58 +0000

    Improved handling of TimThumb thumbnails

--- helpers/timthumb.php
+++ helpers/timthumb.php
@@ -5,8 +5,14 @@
  */
 
 function skt_get_timbhumb_url($url, $args = array()) {
-	$thumburl = plugins_url("skt-fundaments/vendor/timthumb.php", 'skt-fundaments');
-	$thumburl .= '?src=' . urlencode($url);
+	$uploads = wp_upload_dir();
+	$url = str_replace($uploads['baseurl'], $uploads['basedir'], $url);
+	
+	if(substr($url, 0, strlen(ABSPATH)) == ABSPATH) {
+		$url = substr($url, strlen(ABSPATH));
+	}
+	
+	$thumburl = plugins_url("skt-fundaments/vendor/timthumb.php", 'skt-fundaments') . '?src=' . urlencode($url);
 	
 	if(is_array($args) && count($args) > 0) {
 		$thumburl .= '&' . http_build_query($args);
--- vendor/timthumb-config.php
+++ vendor/timthumb-config.php
@@ -1,1 +1,17 @@
-<?php define('FILE_CACHE_DIRECTORY', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/timthumb.cache');
+<?php $current_root = realpath(dirname(__file__) . '/../../../../');
+$doc_root = $_SERVER['DOCUMENT_ROOT'];
+
+if($current_root != $doc_root) {
+	error_log(
+		'Document root from the auto-discovered directory. ' .
+		'This will only work for sites where WordPress is installed in the web root directory.'
+	);
+	
+	define('FILE_CACHE_DIRECTORY',
+		$doc_root . '/wp-content/timthumb.cache'
+	);
+} else {
+	define('FILE_CACHE_DIRECTORY',
+		$current_root . '/wp-content/timthumb.cache'
+	);
+}
