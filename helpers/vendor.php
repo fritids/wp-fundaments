5940f3efa82007249070aff458dbad2cb7275e74
Mark Steadman <marksteadman@me.com>
2013-11-14 14:50:36 +0000

    Removed unnecessary files

--- helpers/vendor.php
+++ helpers/vendor.php
@@ -1,4 +1,8 @@
-<?php
+<?phpp /**
+ * Shortcut functions to inclusion of third-party PHP files
+ *
+ * @package vendorwp-fundaments
+ */
 
 function skt_vendor_include($name) {
 	$GLOBALS['skt_fundaments']->include_vendor_library($name);
