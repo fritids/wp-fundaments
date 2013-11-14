478fc4ecd8d64ed6124a78bae2a1a9518538a491
Mark Steadman <marksteadman@me.com>
2013-11-14 11:47:28 +0000

    Updated TimThumb handling

--- vendor/timthumb-config.php
+++ vendor/timthumb-config.php
@@ -1,17 +1,3 @@
-<?php $current_root = realpath(dirname(__file__) . '/../../../../');
-$doc_root = $_SERVER['DOCUMENT_ROOT'];
-
-if($current_root != $doc_root) {
-	error_log(
-		'Document root from the auto-discovered directory. ' .
-		'This will only work for sites where WordPress is installed in the web root directory.'
-	);
-	
-	define('FILE_CACHE_DIRECTORY',
-		$doc_root . '/wp-content/timthumb.cache'
-	);
-} else {
-	define('FILE_CACHE_DIRECTORY',
-		$current_root . '/wp-content/timthumb.cache'
-	);
-}
+<?php define('FILE_CACHE_DIRECTORY',
+	realpath(dirname(__file__) . '/../../../../wp-content/timthumb.cache')
+);
