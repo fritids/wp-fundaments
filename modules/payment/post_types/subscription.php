d063d168b437bccb54382421f39240ad1775407f
Steadman <mark+github@steadman.io>
2014-02-28 12:06:45 +0000

    Made payment fields read-only

--- modules/payment/post_types/subscription.php
+++ modules/payment/post_types/subscription.php
@@ -1,21 +1,24 @@
 <?php class SubscriptionPostType extends SktPostType {
 	protected $queryable = false;
 	protected $fields = array(
-		'token', 'status',
+		'token' => array('readonly' => 'readonly'),
+		'status' => array('readonly' => 'readonly'),
+		'transaction_id' => array('label' => 'Transaction ID', 'readonly' => 'readonly'),
 		'provider' => array(
-			'type' => 'provider:_theme:payment'
+			'type' => 'provider:_theme:payment',
+			'readonly' => 'readonly'
 		),
 		'items' => array(
 			'type' => 'fieldset',
 			'fields' => array(
-				'name',
-				'amount' => array('type' => 'float'),
-				'description' => array('type' => 'textarea')
+				'name' => array('readonly' => 'readonly'),
+				'amount' => array('type' => 'float', 'readonly' => 'readonly'),
+				'description' => array('type' => 'textarea', 'readonly' => 'readonly')
 			)
 		),
-		'cancel_url' => array('label' => 'Cancel URL'),
-		'return_url' => array('label' => 'Return URL'),
-		'start' => array('type' => 'datetime'),
+		'cancel_url' => array('label' => 'Cancel URL', 'readonly' => 'readonly'),
+		'return_url' => array('label' => 'Return URL', 'readonly' => 'readonly'),
+		'start' => array('type' => 'datetime', 'readonly' => 'readonly'),
 		'period' => array(
 			'type' => 'select',
 			'choices' => array(
@@ -23,9 +26,10 @@
 				'w' => 'Week',
 				'm' => 'Month',
 				'y' => 'Year'
-			)
+			),
+			'readonly' => 'readonly'
 		),
-		'total' => array('type' => 'float'),
+		'total' => array('type' => 'float', 'readonly' => 'readonly'),
 		'trial_period' => array(
 			'type' => 'select',
 			'choices' => array(
@@ -33,16 +37,17 @@
 				'w' => 'Week',
 				'm' => 'Month',
 				'y' => 'Year'
-			)
+			),
+			'readonly' => 'readonly'
 		),
-		'trial_amount' => array('type' => 'number'),
-		'currency' => array('default' => 'GBP'),
-		'country' => array('default' => 'UK')
+		'trial_amount' => array('type' => 'number', 'readonly' => 'readonly'),
+		'currency' => array('default' => 'GBP', 'readonly' => 'readonly'),
+		'country' => array('default' => 'UK', 'readonly' => 'readonly')
 	);
 	
 	protected $meta_boxes = array(
 		'payment' => array(
-			'fields' => array('token', 'start', 'period', 'total', 'currency', 'country')
+			'fields' => array('token', 'transaction_id', 'start', 'period', 'total', 'currency', 'country')
 		),
 		'trial' => array(
 			'fields' => array('trial_period', 'trial_amount')
@@ -53,7 +58,7 @@
 		)
 	);
 	
-	protected $list_fields = array('token', 'status', 'provider');
+	protected $list_fields = array('transaction_id', 'status', 'provider');
 	protected $supports = array('author', 'editor', 'excerpt');
 	protected $can_add = false;
 }
