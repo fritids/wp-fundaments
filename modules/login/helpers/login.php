d20384c363290380f0381db2682a3a6a7ff49c09
Steadman <mark+github@steadman.io>
2014-05-05 14:52:29 +0100

    Removed nonce check from login form

--- modules/login/helpers/login.php
+++ modules/login/helpers/login.php
@@ -1,41 +1,37 @@
 <?php function skt_login_form() {
 	$redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : admin_url();
 	
-	if (!empty($_POST) && !wp_verify_nonce($_POST['skt-fundaments-login'], basename(__file__))) {
-		wp_die('Not a chance!');
-	}
-	
 	if (is_ssl() && force_ssl_login() && !force_ssl_admin() && (strpos($redirect_to, 'https') != 0) && (strpos($redirect_to, 'http') != 0)) {
 		$secure_cookie = false;
 	} else {
 		$secure_cookie = '';
 	}
-	
+
 	$user = wp_signon('', $secure_cookie);
 	$redirect_to = apply_filters('login_redirect',
 		$redirect_to,
 		isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : '',
 		$user
 	);
-	
+
 	if (!is_wp_error($user)) {
 		if (!$user->has_cap('edit_posts') && (empty($redirect_to) || $redirect_to == 'wp-admin/')) {
 			$redirect_to = admin_url('profile.php');
 		}
-		
+
 		wp_safe_redirect($redirect_to);
 		exit();
 	}
-	
+
 	$errors = $user;
 	if (!empty($_GET['loggedout'])) {
 		$errors = new WP_Error();
 	}
-	
+
 	if (isset($_POST['testcookie']) && empty($_COOKIE[TEST_COOKIE])) {
-		$errors->add('test_cookie', __("Cookies are blocked or not supported by your browser. You must <a href='http://www.google.com/cookies.html'>enable cookies</a> to use WordPress."));		
+		$errors->add('test_cookie', __("Cookies are blocked or not supported by your browser. You must <a href='http://www.google.com/cookies.html'>enable cookies</a> to use WordPress."));
 	}
-	
+
 	if (isset($_GET['loggedout']) && $_GET['loggedout']) {
 		$errors->add('loggedout', __('You are now logged out.'), 'message');
 	} elseif (isset($_GET['registration']) && $_GET['registration'] == 'disabled') {
@@ -47,20 +43,20 @@
 	} elseif (isset($_GET['checkemail']) && $_GET['checkemail'] == 'registered') {
 		$errors->add('registered', __('Registration complete. Please check your e-mail.'), 'message');
 	}
-	
+
 	$GLOBALS['skt_login_errors'] = $errors;
 	add_action('skt_login_form', 'skt_login_form_print');
 	$login_path = get_template_directory(). '/wp-login.php';
-	
+
 	if(isset($_GET['loggedout']) && $_GET['loggedout'] == 'true') {
 		$logged_out_path = get_template_directory(). '/wp-logout.php';
-		
+
 		if(is_file($logged_out_path)) {
 			include($logged_out_path);
 			return;
 		}
 	}
-	
+
 	if(is_file($login_path)) {
 		include($login_path);
 	}
@@ -71,12 +67,12 @@ function skt_login_form_print() {
 	if (isset($_GET['action']) && $_GET['action'] != 'login') {
 		return;
 	} ?>
-	
+
 	<form class="loginform" action="<?php bloginfo('wpurl'); ?>/wp-login.php" method="post">
 		<?php skt_open_signup_fieldset(
 			apply_filters('skt_signup_fieldset1_title', '')
 		);
-		
+
 		if(!defined('SKT_USERNAME_AUTH') || SKT_USERNAME_AUTH) {
 			skt_signup_field('log',
 				array(
@@ -93,16 +89,16 @@ function skt_login_form_print() {
 				)
 			);
 		}
-		
+
 		skt_signup_field('pwd',
 			array(
 				'label' => apply_filters('skt_signup_field_label', __('Password'), 'pwd'),
 				'type' => 'password'
 			)
 		);
-		
+
 		skt_close_signup_fieldset(); ?>
-		
+
 		<p class="submit">
 			<?php wp_nonce_field(basename(__file__), 'skt-fundaments-login'); ?>
 			<input type="submit" name="wp-submit" id="wp-submit" value="<?php _e('Login'); ?>" />
@@ -116,13 +112,13 @@ function skt_authenticate_email($username) {
 	if(defined('SKT_USERNAME_AUTH') && !SKT_USERNAME_AUTH) {
 		if(empty($_GET['interim-login']) || $_GET['interim-login'] != '1') {
 			$user = get_user_by_email($username);
-			
+
 			if(!empty($user->user_login)) {
 				$username = $user->user_login;
 			}
 		}
 	}
-	
+
 	return $username;
 }
 
@@ -132,7 +128,7 @@ function skt_login_redirect($user_login, $user) {
 		wp_redirect(
 			apply_filters('skt_login_redirect_url', admin_url('profile.php'))
 		);
-		
+
 		die();
 	}
 }
